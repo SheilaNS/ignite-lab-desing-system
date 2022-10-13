6f64c4663e0e529c406c43dc161f568b64548de5
SheilaNS <shei.nsantos@gmail.com>
2022-10-12 21:31:06 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className="font-bold text-2xl">Hello World!</h1>
+      <button className="bg-cyan-500 font-medium px-4 py-2 rounded hover:bg-cyan-300">
+        Enviar
+      </button>
+    </>
+  )
+}
